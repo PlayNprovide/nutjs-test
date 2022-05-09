82ab42b700dc56ecaec90224345c0c2c6057f0e5
PlayNprovide <playnprovide@gmail.com>
2022-05-10 01:23:24 +0900

    modified logger

--- src/main/logger.ts
+++ src/main/logger.ts
@@ -3,8 +3,27 @@ import path from 'path'
 import winston from 'winston'
 
 export const logger = winston.createLogger({
-  transports: [new winston.transports.Console()]
+  transports: [
+    new winston.transports.File({
+      filename: path.join(app.getPath('userData'), 'logs.txt'),
+      format: winston.format.printf(
+        ({ level, source, message, ...meta }) => `${level} [${source}]: ${message} ${JSON.stringify(meta)}`
+      )
+    })
+  ]
 })
 
-if (process.env.NODE_ENV === 'production')
-  logger.add(new winston.transports.File({ filename: path.join(app.getPath('userData'), 'log.txt') }))
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format((info) => ({ ...info }))(),
+        winston.format.colorize({ level: true }),
+        winston.format.printf((info) => {
+          const { source, level, message, ...meta } = info
+          return `${level} [${source}]: ${message} ${JSON.stringify(meta)}`
+        })
+      )
+    })
+  )
+}
