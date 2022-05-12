8e654488a2e9d43702267e65242a9a3005e6102f
PlayNprovide <playnprovide@gmail.com>
2022-05-12 16:18:17 +0900

    misc

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,14 @@
 import { app, BrowserWindow, dialog, ipcMain, shell } from 'electron'
+import { webId } from 'lib/ipc-event'
 import { WinRef } from 'lib/type'
 import * as path from 'path'
 import { config } from './config'
 import { logger } from './logger'
 
-const mainWindow: WinRef = { current: null }
+const userWindow: WinRef = { current: null }
 
-function createMainWindow() {
-  mainWindow.current = new BrowserWindow({
+function createUserWindow() {
+  userWindow.current = new BrowserWindow({
     show: false,
     minWidth: 800,
     minHeight: 600,
@@ -26,23 +27,23 @@ function createMainWindow() {
     }
   })
 
-  mainWindow.current.loadFile(`${__dirname}/index.html`)
+  webId.set('USER', userWindow.current.webContents.id)
+  userWindow.current.loadFile(`${__dirname}/index.html`)
 
-  mainWindow.current.on('ready-to-show', mainWindow.current.show)
-  mainWindow.current.webContents.setWindowOpenHandler(({ url, disposition }) => {
-    console.log('window is openning', disposition)
+  userWindow.current.on('ready-to-show', userWindow.current.show)
+  userWindow.current.webContents.setWindowOpenHandler(({ url, disposition }) => {
+    logger.info('window is openning', { disposition, source: 'userWindow' })
     shell.openExternal(url)
     return { action: 'deny' }
   })
 
-  mainWindow.current.on('closed', () => {})
+  userWindow.current.on('closed', () => {})
 }
 
 async function registerListeners() {
   // 콜백이 등록되는 순간에는 윈도우가 초기화되지않은 경우가 있을 수 있다
   // 이 경우 직접 함수의 레퍼런스를 넘기는 방식으로 콜백을 등록하면 destroyed 에러가 난다
   // 따라서 윈도우의 초기화가 보장되지 않는다면 익명함수 내에서 처리할 것
-  if (mainWindow.current === null) return new Error('windows not initialized')
   ipcMain.on('message', (e, ...args) => {
     console.log(`got a message from ${e.sender.id}, ${e.processId}, ${e.frameId}, ${e.ports} : ${args}`)
   })
@@ -55,28 +56,25 @@ async function registerListeners() {
   })
 
   ipcMain.on('select-folder', (e) => {
-    if (mainWindow.current)
+    if (userWindow.current)
       dialog
-        .showOpenDialog(mainWindow.current, { properties: ['openDirectory'] })
+        .showOpenDialog(userWindow.current, { properties: ['openDirectory'] })
         .then(({ canceled, filePaths }) => !canceled && e.reply('path', ...filePaths))
   })
 
   ipcMain.on('get-path', (e, name) => e.reply('path', app.getPath(name)))
 
-  ipcMain.on('focus', () => mainWindow.current?.show())
+  ipcMain.on('focus', () => userWindow.current?.show())
 
   ipcMain.on('load-url', (e, _url: string) => BrowserWindow.fromWebContents(e.sender)?.loadURL(_url))
 }
 
-app
-  .on('ready', createMainWindow)
-  .whenReady()
-  .then(registerListeners)
-  .then(() => {
-    config.set('hello', 'world')
-    logger.log('info', 'app is ready')
-  })
-  .catch((e) => console.error(e))
+app.on('ready', () => {
+  createUserWindow()
+  registerListeners()
+  config.set('hello', 'world')
+  logger.log('info', 'app is ready')
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -86,6 +84,6 @@ app.on('window-all-closed', () => {
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createMainWindow()
+    createUserWindow()
   }
 })
