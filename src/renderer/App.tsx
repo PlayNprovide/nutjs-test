2fdee9b5c0df6edd2fd6a178aac9ff6a2bbdfb51
PlayNprovide <playnprovide@gmail.com>
2022-05-18 17:06:08 +0900

    added routers

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/pages/Home'
 
 const App = () => {
-  return <div>App</div>
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      {/* <Route path='/auth' element={ <Auth/> } />  */}
+    </Routes>
+  )
 }
 
 export default App
